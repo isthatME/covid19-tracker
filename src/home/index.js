21a1629fb3fed242f5b4fd35efe5c594590352ea
Guilherme <guigo_gaara@hotmail.com>
2020-11-13 17:03:13 -0300

    table done w/ no style

--- src/home/index.js
+++ src/home/index.js
@@ -5,16 +5,28 @@ import { FaSkull, FaCheck } from "react-icons/fa";
 import { GiLifeBar } from "react-icons/gi";
 
 function Index() {
-    const [state, setState] = useState([])
+    const [stateHighLighted, setStateHighLigth] = useState([])
+    const [filteredTable, setFilteredTable] = useState([])
+    const [tableQuery, setTableQuery] = useState('');
+
 
     useEffect(() => {
         async function fetchData() {
-            const req = await Axios.get('https://covid19-brazil-api.now.sh/api/report/v1/countries/')
-            setState(req.data.data)
+            const req = await Axios.get(`https://covid19-brazil-api.now.sh/api/report/v1/countries`)
+            setStateHighLigth(req.data.data)
             return req
         }
         fetchData();
     }, [])
+    useEffect(() => {
+        async function fetchQuery() {
+            const req = await Axios.get(`https://covid19-brazil-api.now.sh/api/report/v1/${tableQuery}`)
+            setFilteredTable(req.data.data)
+            return req
+        }
+        fetchQuery();
+    }, [tableQuery])
+
 
     return (
         <div className="content">
@@ -23,7 +35,7 @@ function Index() {
                     <h1>Estatistica em alguns países</h1>
                 </div>
                 <div className="card-wrapper">
-                    {state.filter(e => e.country === 'Brazil' || e.country === 'Italy' || e.country === 'China' || e.country === 'Argentina').map(filteredCountry => (
+                    {stateHighLighted.filter(e => e.country === 'Brazil' || e.country === 'Italy' || e.country === 'China' || e.country === 'Argentina').map(filteredCountry => (
                         <div className="country-card">
                             <div className="country-stats">
                                 <h1>Covid Status: {filteredCountry.country}</h1>
@@ -46,8 +58,34 @@ function Index() {
                     ))}
                 </div>
             </section>
-            <section className="table-filter">
-
+            <section className="card-table">
+                <div className="card-table-filter">
+                    <h1>Mais informações</h1>
+                    <select id="" onChange={e => setTableQuery(e.target.value)}>
+                        <option value="">Estados</option>
+                        <option value="countries">Países</option>
+                    </select>
+                </div>
+                <div className="card-table-wrapper">
+                    <table className="content-table">
+                        <thead>
+                            <tr>
+                                {tableQuery === '' ? <th>Estados</th> : <th>Países</th>}
+                                <th>Confirmados</th>
+                                <th>Mortes</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredTable.map(filteredCoutryState => (
+                                <tr>
+                                    {tableQuery === '' ? <td>{filteredCoutryState.state}</td> : <td>{filteredCoutryState.country}</td>}
+                                <td>{filteredCoutryState.cases}</td>
+                                <td>{filteredCoutryState.deaths}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </section>
 
         </div>
