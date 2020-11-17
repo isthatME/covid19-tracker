04e14e79638d4064e2f4bda320da374126eab4c4
Guilherme <guigo_gaara@hotmail.com>
2020-11-17 18:56:05 -0300

    table style update

--- src/home/index.js
+++ src/home/index.js
@@ -2,13 +2,23 @@ import Axios from 'axios'
 import '../home/style.css'
 import React, { useEffect, useState } from 'react'
 import '../home/style.css'
-import { FaSkull, FaCheck } from "react-icons/fa";
-import { GiLifeBar } from "react-icons/gi";
+import { FaSkull, FaCheck, FaLocationArrow } from "react-icons/fa";
+import { GiLifeBar, GiPadlock, GiMoneyStack, GiBed } from "react-icons/gi";
 
 function Index() {
     const [stateHighLighted, setStateHighLigth] = useState([])
     const [filteredTable, setFilteredTable] = useState([])
     const [tableQuery, setTableQuery] = useState('');
+    const [title, setTitle] = useState('')
+    var teste;
+
+
+    function handleUpdate() {
+        teste = filteredTable.filter(e => e.uf === title.toUpperCase())
+        console.log(teste)
+        teste[0].state = "SERGIPE"
+        console.log(teste)
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -25,7 +35,7 @@ function Index() {
             return req
         }
         fetchQuery();
-    }, [tableQuery])
+    }, [tableQuery, teste])
 
 
     return (
@@ -71,18 +81,47 @@ function Index() {
                         <table className="content-table sticky">
                             <thead>
                                 <tr>
-                                    {tableQuery === '' ? <th>Estados</th> : <th>Países</th>}
-                                    <th>Confirmados</th>
-                                    <th>Mortes</th>
-                                    <th>Dias em lockDown</th>
+
+                                    {tableQuery === '' ?
+                                        <th>
+                                            <FaLocationArrow className="location-icon"></FaLocationArrow>
+                                                        Estados
+                                        </th> :
+                                        <th>
+                                            <FaLocationArrow></FaLocationArrow>
+                                                        Países
+                                        </th>}
+
+                                    <th>
+                                        <FaCheck className="confirmed-icon"></FaCheck>
+                                            Confirmados
+                                    </th>
+                                    <th>
+                                        <FaSkull className="deaths-icon"></FaSkull>
+                                            Mortes
+                                    </th>
+                                    <th>
+                                        <GiPadlock className="lockdown-icon"></GiPadlock>
+                                            Dias em lockDown
+                                    </th>
+                                    <th>
+                                        <GiMoneyStack className="money-icon"></GiMoneyStack>
+                                            Total gasto
+                                    </th>
+                                    <th>
+                                        <GiBed className="leitos-icon"></GiBed>
+                                            Leitos ocupados
+                                    </th>
                                 </tr>
                             </thead>
-                            <tbody>
+                            <tbody >
                                 {filteredTable.map(filteredCoutryState => (
                                     <tr key={filteredCoutryState.uf}>
                                         {tableQuery === '' ? <td>{filteredCoutryState.state}</td> : <td>{filteredCoutryState.country}</td>}
                                         <td>{filteredCoutryState.cases}</td>
                                         <td>{filteredCoutryState.deaths}</td>
+                                        <td>{Math.floor(Math.random() * (300 - 40) + 40)}</td>
+                                        <td>R$: {Math.floor(Math.random() * (60000000 - 20000000) + 20000000)}</td>
                                         <td>{Math.floor(Math.random() * (300 - 40) + 40)}</td>
                                     </tr>
                                 ))}
@@ -90,8 +129,14 @@ function Index() {
                         </table>
                     </div>
                 </div>
+                <div className="top-five-cards">
+                </div>
             </section>
 
+
+
+            <input type="text" onChange={e => setTitle(e.target.value)} />
+            <button onClick={handleUpdate}>update</button>
         </div>
     );
 }
